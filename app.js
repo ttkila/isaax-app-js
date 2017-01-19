b8b98a4e6b3043dde6ab4ef6895dc41371598339
Kylych Tynybekov <ktynybekov@gmail.com>
2017-01-19 18:05:18 +0600

    reduced message

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 setInterval(function() {
-  console.log("Start")
   console.log("***********************");
   console.log("  New version message  ");
   console.log("***********************");
-  console.log("x_x")
-  console.log("o_O")
-  console.log("x_x")
-  console.log(" end of message ")
-  console.log("Bye!")
-  console.log("End")
-}, 10000);
+}, 90000);
