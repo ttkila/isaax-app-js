a8aa68994b82605a43f8b02c7995afd63c28cadb
Kylych Tynybekov <ktynybekov@gmail.com>
2017-02-22 08:02:38 +0600

    added

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ setInterval(function() {
   console.log("xxx some important message xxx");
   console.log("xxx some important message xxx");
   console.log("xxx some important message xxx");
+  console.log("xxx some important message xxx");
 }, 10000);
