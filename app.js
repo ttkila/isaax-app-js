bbdda2cff8dda32fab174e7176bd46d0392bbd28
Kylych Tynybekov <ktynybekov@gmail.com>
2017-04-05 15:11:26 +0600

    removed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 setInterval(function() {
  console.log("XXX some important message XXX");
  console.log("XXX some important message XXX");
+ console.log("XXX some important message XXX");
 }, 10000);
